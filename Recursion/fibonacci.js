4f46d62200ad9dce9e2f79076fb94d02b8c0ebab
Shankar <52577232+AVShankar@users.noreply.github.com>
2019-12-09 00:09:18 +0530

    Update fibonacci.js

--- Recursion/fibonacci.js
+++ Recursion/fibonacci.js
@@ -6,11 +6,27 @@ const prompts = rl.createInterface(
 )
 
 prompts.question('Enter the nth number: ', value => {
-    fibo(parseInt(value))
+    let res=[]
+    for(let i=0; i<value; i++)
+    {
+        res.push(fibo(i))
+    }
+    console.log(res.join(' '))
     process.exit()
 })
 
 function fibo(n)
 {
-    
+    if(n==0)
+    {
+        return 0
+    }
+    else if(n==1)
+    {
+        return 1
+    }
+    else
+    {
+        return (fibo(n-1) + fibo(n-2))
+    }
 }
